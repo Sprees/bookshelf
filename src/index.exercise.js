f5aff7c8080fe431e00dbf89c2e998d132c4ff68
Sprees <Alexrminnick@gmail.com>
2022-11-21 11:12:36 -0700

    extra credit 1 complete

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 
+import '@reach/dialog/styles.css'
+import {Dialog} from '@reach/dialog'
+
 import {Logo} from './components/logo'
 
 const App = () => {
+  const [openModal, setOpenModal] = React.useState('none')
+
   return (
     <div>
       <Logo height="80" width="80" />
-      <button onClick={() => alert("You've pressed me!")}>Login</button>
-      <button onClick={() => alert("You've pressed me!")}>Register</button>
+      <button onClick={() => setOpenModal('login')}>Login</button>
+      <button onClick={() => setOpenModal('register')}>Register</button>
+
+      {openModal === 'login' && (
+        <Dialog>
+          <p>This is the Login Modal</p>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </Dialog>
+      )}
+
+      {openModal === 'register' && (
+        <Dialog>
+          <p>This is the Register Modal</p>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </Dialog>
+      )}
     </div>
   )
 }
